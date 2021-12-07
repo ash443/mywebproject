5dd5b67bde0d59e8df09f9e7ec0fbfd11399f4af
akshaymg29 <akshaymg29@gmail.com>
2021-12-07 00:50:24 -0500

    Modified hotel page

--- js/hotel.js
+++ js/hotel.js
@@ -1,3 +1,23 @@
+"use strict";
+$(document).ready( () => {
+    let nextSlide = $("#slides img:first-child");
+        
+    // start slide show
+    setInterval( () => {
+        $("#slide").fadeOut(1000, () => {
+                if (nextSlide.next().length == 0) {
+                    nextSlide = $("#slides img:first-child");
+                }
+                else {
+                    nextSlide = nextSlide.next();
+                }
+                const nextSlideSource = nextSlide.attr("src");
+                $("#slide").attr("src", nextSlideSource).fadeIn(1000);
+            });    // end fadeOut()
+    },
+    5000);         // end setInterval()
+});
+
 function onNavFlightClick() {
     window.location.href = "flight.html";
 }
