e4045e28ac83925edbc64df01c60a545cec138f8
akshaymg29 <akshaymg29@gmail.com>
2021-12-06 23:33:55 -0500

    Added feature two: Hotels page
    Added hotels  page with .js and .css

---
+++ js/hotel.js
@@ -0,0 +1,11 @@
+function onNavFlightClick() {
+    window.location.href = "flight.html";
+}
+
+function onNavHotelClick() {
+    window.location.href = "hotel.html";
+}
+
+function onNavHolidayHomesClick() {
+    window.location.href = "homes.html";
+}
